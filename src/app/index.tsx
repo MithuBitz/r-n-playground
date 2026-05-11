8168e3da8962ec2ba63f50c5da3d13a4648c936b
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2026-05-12 00:05:19 +0530

    style flatten

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,42 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+
+const styleA = StyleSheet.create({ text: { color: "red", fontSize: 16 } });
+
+const styleB = StyleSheet.create({
+  text: { fontSize: 24, fontWeight: "bold" },
+});
+
+const flat = StyleSheet.flatten([styleA.text, styleB.text])
 
 const HomeScreen = () => {
-  const isActive = true;
-
-  // Compose style
-  const buttonStyle = StyleSheet.compose(
-    styles.button,
-    isActive ? styles.activeButton : null,
-  );
-
-  return (
-    <View style={styles.container}>
-      {/* <View style={[styles.button, isActive && styles.activeButton]}> */}
-      {/* @ts-ignore */}
-      <View style={buttonStyle}>
-        <Text style={styles.buttonText}>Composed Style</Text>
-      </View>
-    </View>
-  );
+  return <Text style={flat}>Flattend Style</Text>;
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  button: {
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 10,
-    backgroundColor: "#ccc",
-  },
-  activeButton: {
-    backgroundColor: "#6C63FF",
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-});
+const styles = StyleSheet.create({});
